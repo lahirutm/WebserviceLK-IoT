5c33e98b30783dac1d7bdffd35590cd5e4381101
Lahiru Maduranga <lahirutm@gmail.com>
2022-07-27 16:55:33 +0530

    Update connect_example.js

--- nodejs_examples/connect_example.js
+++ nodejs_examples/connect_example.js
@@ -1,30 +1,36 @@
+// Got help from Kakopappa's Sinric repository
+
 // Get help from Kakopappa's Sinric repository
 
 const WebSocket = require('ws');
-const token = "Your api key from iot.webservice.lk";
+const token = 'Your api key from iot.webservice.lk';
+const device_id = 'Your device id fro iot.webservice.lk';
 
-const options = {
-    headers: {
-        "Authorization" : Buffer.from("apikey:" + token).toString('base64')
-    }
-};
+const ws = new WebSocket('ws://'+token+':'+device_id+'@iot.webservice.lk:8080');
 
-const ws = new WebSocket('ws://iot.webservice.lk:8080', options);
- 
-ws.on('open', function open() {
-   console.log("Connected with iot.WebserviceLK. waiting for commands..");
-});
+ws.on('open', function open () {
+  console.log('Connected with iot.webservice.lk. waiting for commands..');
+})
 
-ws.on('message', function incoming(data) {
-   console.log("Request : " + data);
-   let jsonObj = JSON.parse(data);
+ws.on('message', function incoming (data) {
+  console.log(data);
+  var jsonData = JSON.parse(data)
+  console.log(jsonData);
+  if (jsonData.deviceId === device_id) {
+    if (jsonData.action === 'setPowerState') {
+      // command: "Ok Google turn on MY_DEVICE"
+      // {"deviceId":"8xxxxxxxxxxxxxxxxxxxxxx","action":"setPowerState","value":"ON"}
+      // command: "Ok Google turn off MY_DEVICE"
+      // {"deviceId":"8xxxxxxxxxxxxxxxxxxxxxx","action":"setPowerState","value":"OFF"}
+      if (jsonData.value === 'ON') {
+	       // here you can handle signal sending to your hadware (A Relay or Something)
+        console.log('Turning ON MY_DEVICE...');
+      } else {
+	       // here you can handle signal sending to your hadware (A Relay or Something)
+        console.log('Turning OFF MY_DEVICE...');
+      }
+    } 
+  }
+})
 
-   if(jsonObj.action == "setPowerState") {
-       if(jsonObj.value === "ON") {
-           console.log("Turn on device");
-       } else {
-           console.log("Turn off device");
-       } 
-   }
 
-});
